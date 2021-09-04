de812164c52c1683ecf30ea96c15f0e5490d9319
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-09-04 19:30:02 -0300

    initial commit

---
+++ src/constants/colors.ts
@@ -0,0 +1,13 @@
+export default {
+    background: "#252626",
+    text: "#212121",
+    yellow: "#F2FE8D",
+    mint: "#B2D0CE",
+    lilac: "#AA9EB7",
+    gray1: "#252525",
+    gray2: "#212121",
+    gray3: "#79767D",
+    mintGradient: "#EAEAEA",
+    yellowGradient: "#FCFFDF",
+    lilacGradient: "#F2EFF4"
+}
