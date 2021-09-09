484e504fd7fb5e4702d4a7f61e9a85af7ef2576d
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-09-09 20:06:38 -0300

    chore: modify and add new colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,13 +1,15 @@
-export default {
-    background: "#252626",
-    text: "#212121",
-    yellow: "#F2FE8D",
-    mint: "#B2D0CE",
-    lilac: "#AA9EB7",
-    gray1: "#252525",
-    gray2: "#212121",
-    gray3: "#79767D",
-    mintGradient: "#EAEAEA",
-    yellowGradient: "#FCFFDF",
-    lilacGradient: "#F2EFF4"
+export const colors = {
+  background: "#1E2121",
+  primary: "#FFA500",
+  text: "#212121",
+  white: "#FFFFFF",
+  yellow: "#F2FE8D",
+  mint: "#B2D0CE",
+  lilac: "#AA9EB7",
+  gray1: "#252525",
+  gray2: "#212121",
+  gray3: "#79767D",
+  mintGradient: "#EAEAEA",
+  yellowGradient: "#FCFFDF",
+  lilacGradient: "#F2EFF4"
 }
